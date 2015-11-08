9381bf5c9087342f116af434f4e022c99308b600
Harry Oliver <someonecancode@yahoo.co.uk>
2015-11-08 12:03:39 +0000

    Add InventoryModule and stock methods

--- store.rb
+++ store.rb
@@ -1,8 +1,20 @@
 module InventoryModule
   
+  def stock_count
+    @stock_count ||= 0
+  end
+  
+  def stock_count=(number)
+    @stock_count = number
+  end
+  
+  def in_stock?
+    stock_count > 0
+  end
 end
 
 class Shirt
+  include InventoryModule
   attr_accessor :attributes
   
   def initialize(attributes)
@@ -28,3 +40,9 @@ end
 
 shirt = Shirt.new(name: "POLO", size: "L")
 shirt2 = Shirt.new(name: "POLO", size: "M")
+
+shirt.stock_count = 10
+
+puts "Shirt 1 stock count: %s" % shirt.stock_count
+puts "Shirt 2 stock count: %s" % shirt2.stock_count
+puts "Shirt 1 in stock? %s" % shirt.in_stock?
