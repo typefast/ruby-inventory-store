2abd1cbd8da20b062474014365387ad16a68c181
Harry Oliver <someonecancode@yahoo.co.uk>
2015-11-08 12:56:35 +0000

    ProductItems namespace, reoprt method refactor

--- store.rb
+++ store.rb
@@ -20,10 +20,9 @@ module InventoryModule
       instances.each(&block)
     end
     
-    def in_stock_report
-      puts "#{self.to_s} In Stock Report"
-      reportable = instances.select{ |instance| instance.in_stock? }
-      reportable.each do |item|
+    def report(title, items)
+      puts title
+      items.each do |item|
         line = []
         line.push("item: #{item.attributes[:name]}")
         line.push("Stock: #{item.stock_count}")
@@ -33,6 +32,18 @@ module InventoryModule
         puts line.join("\t")
       end
       puts "\n"
+    end
+    
+    def in_stock_report
+      title = "#{self.to_s} In Stock Report"
+      reportable = instances.select{ |instance| instance.in_stock? }
+      report(title, reportable)
+    end
+    
+    def out_of_stock_report
+      title = "#{self.to_s} Out Of Stock Report"
+      reportable = instances.select{ |instance| !instance.in_stock? }
+      report(title, reportable)
     end
   end
   
@@ -48,51 +59,58 @@ module InventoryModule
     stock_count > 0
   end
 end
-
-class Shirt
-  include InventoryModule
-  attr_accessor :attributes
+module ProductItems
+  class Shirt
+    include InventoryModule
+    attr_accessor :attributes
+    
+    def initialize(attributes)
+      @attributes = attributes
+    end
+  end
   
-  def initialize(attributes)
-    @attributes = attributes
+  class Trousers
+    include InventoryModule
+    attr_accessor :attributes
+    
+    def initialize(attributes)
+      @attributes = attributes
+    end
+  end
+  
+  class Accessory
+    include InventoryModule
+    attr_accessor :attributes
+    
+    def initialize(attributes)
+      @attributes = attributes
+    end
   end
 end
 
-class Trousers
-  include InventoryModule
-  attr_accessor :attributes
-  
-  def initialize(attributes)
-    @attributes = attributes
-  end
-end
-
-class Accessory
-  include InventoryModule
-  attr_accessor :attributes
-  
-  def initialize(attributes)
-    @attributes = attributes
-  end
-end
-
-shirt1 = Shirt.create(name: "POLO", size: "L")
-shirt2 = Shirt.create(name: "POLO", size: "M")
+shirt1 = ProductItems::Shirt.create(name: "POLO", size: "L")
+shirt2 = ProductItems::Shirt.create(name: "POLO", size: "M")
 shirt1.stock_count = 10
-shirt2.stock_count = 3
+shirt2.stock_count = 0
 
-jeans = Trousers.create(name: "Jeans", size: "32")
+jeans = ProductItems::Trousers.create(name: "Jeans", size: "32")
 jeans.stock_count = 4
 
-accessory = Accessory.create(name: "Belt", size: "M")
+accessory = ProductItems::Accessory.create(name: "Belt", size: "M")
 accessory.stock_count = 7
 
-accessory = Accessory.create(name: "Necklace")
+accessory = ProductItems::Accessory.create(name: "Necklace")
 accessory.stock_count = 2
 
 puts "Shirt 1 stock count: %s" % shirt1.stock_count
 puts "Shirt 2 stock count: %s" % shirt2.stock_count
 puts "Shirt 1 in stock? %s" % shirt1.in_stock?
 
-Shirt.in_stock_report
+ProductItems::Shirt.in_stock_report
+ProductItems::Trousers.in_stock_report
+ProductItems::Accessory.in_stock_report
+
+ProductItems::Shirt.out_of_stock_report
+
+
 
