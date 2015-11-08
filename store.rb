6e94557423f7e3e07aa81894bab4146699c525ce
Harry Oliver <someonecancode@yahoo.co.uk>
2015-11-08 12:44:41 +0000

    Add in_stock_report method to ClassMethods Module

--- store.rb
+++ store.rb
@@ -1,5 +1,10 @@
 module InventoryModule
   
+  def self.included(klass)
+    klass.extend(ClassMethods)
+    klass.extend(Enumerable)
+  end
+  
   module ClassMethods
     def create(attributes)
       object = new(attributes)
@@ -9,6 +14,25 @@ module InventoryModule
     
     def instances
       @instances ||= []
+    end
+    
+    def each(&block)
+      instances.each(&block)
+    end
+    
+    def in_stock_report
+      puts "#{self.to_s} In Stock Report"
+      reportable = instances.select{ |instance| instance.in_stock? }
+      reportable.each do |item|
+        line = []
+        line.push("item: #{item.attributes[:name]}")
+        line.push("Stock: #{item.stock_count}")
+        if item.attributes.include?(:size)
+          line.push("Size: #{item.attributes[:size]}")
+        end
+        puts line.join("\t")
+      end
+      puts "\n"
     end
   end
   
@@ -26,7 +50,6 @@ module InventoryModule
 end
 
 class Shirt
-  extend InventoryModule::ClassMethods
   include InventoryModule
   attr_accessor :attributes
   
@@ -36,6 +59,7 @@ class Shirt
 end
 
 class Trousers
+  include InventoryModule
   attr_accessor :attributes
   
   def initialize(attributes)
@@ -44,6 +68,7 @@ class Trousers
 end
 
 class Accessory
+  include InventoryModule
   attr_accessor :attributes
   
   def initialize(attributes)
@@ -53,9 +78,21 @@ end
 
 shirt1 = Shirt.create(name: "POLO", size: "L")
 shirt2 = Shirt.create(name: "POLO", size: "M")
-
 shirt1.stock_count = 10
+shirt2.stock_count = 3
+
+jeans = Trousers.create(name: "Jeans", size: "32")
+jeans.stock_count = 4
+
+accessory = Accessory.create(name: "Belt", size: "M")
+accessory.stock_count = 7
+
+accessory = Accessory.create(name: "Necklace")
+accessory.stock_count = 2
 
 puts "Shirt 1 stock count: %s" % shirt1.stock_count
 puts "Shirt 2 stock count: %s" % shirt2.stock_count
 puts "Shirt 1 in stock? %s" % shirt1.in_stock?
+
+Shirt.in_stock_report
+
